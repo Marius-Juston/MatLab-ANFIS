0ec242372628da5b21157e16d73dbc5452fac85c
Marius-Juston <Marius.juston@hotmail.fr>
2021-08-02 00:25:45 -0400

    improved python setup

--- python_setup.m
+++ python_setup.m
@@ -1,10 +1,17 @@
 terminate(pyenv)
 
+if exist("pe", "var")
+    terminate(pe)
+end
+
+clear classes
+
 pyExec = 'C:\Users\mariu\Anaconda3\envs\ANFIS\python.exe';
 
 pe = pyenv("ExecutionMode","OutOfProcess");
-if pe.Status == 'NotLoaded'
-    pyenv("ExecutionMode","OutOfProcess","Version", pyExec);
+if pe.Status == 'NotLoaded' || pe.Status == 'Terminated'
+    disp("Reloading enviroenemtn");
+    pe = pyenv("ExecutionMode","OutOfProcess","Version", pyExec);
 end
 
 pyRoot = fileparts(pyExec);
@@ -18,15 +25,32 @@ addToPath = {
     fullfile(pyRoot, 'Library', 'bin')
     fullfile(pyRoot, 'Scripts')
     fullfile(pyRoot, 'bin')
+    fullfile(pwd, 'Python')
+    fullfile(pwd, 'Python', 'anfis')
+    fullfile(pwd, 'Python', 'vizualize')
+    pwd
     };
 p = [addToPath(:); p(:)];
 p = unique(p, 'stable');
 p = strjoin(p, ';');
 setenv('PATH', p);
 
+disp(addToPath);
+
 py.list; % Call a Python function to load interpreter
 pyenv
 
+python_paths = {'' 
+                fullfile(pwd, "Python")
+                fullfile(pwd, "Python", 'anfis') 
+                fullfile(pwd, "Python", 'vizualize')};
+
+for i=1:length(python_paths)
+    name = python_paths{i};
+    if count(py.sys.path, name) == 0
+      insert(py.sys.path, int32(0), name);
+    end
+end
 
 modules = {'numpy' ,'torch'};
 
@@ -34,4 +58,5 @@ for i=1:length(modules)
     module_to_load = modules{i};
     disp(module_to_load);
     python_module_to_use = py.importlib.import_module(module_to_load);
+%    py.importlib.reload(python_module_to_use);
 end

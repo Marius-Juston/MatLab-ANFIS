f6a297fb5c2fe62feb67c18fa9d044ab2101ff2f
Marius-Juston <Marius.juston@hotmail.fr>
2021-08-02 14:35:03 -0400

    Add ONNX model code generator

---
+++ onnx_model_loader.m
@@ -0,0 +1,15 @@
+params = importONNXFunction(modelfile,'ANFISFcn');
+
+file = 'ANFISFcn.m';
+
+fid  = fopen(file,'r');
+f=fread(fid,'*char')';
+fclose(fid);
+f = regexprep(f,'PLACEHOLDER\(([A-Za-z.0-9]+)\)','max(sum(abs($1)))');
+
+fid  = fopen(file,'w');
+fprintf(fid,'%s',f);
+fclose(fid);
+
+
+ANFISFcn([[0,0,0]], params)
